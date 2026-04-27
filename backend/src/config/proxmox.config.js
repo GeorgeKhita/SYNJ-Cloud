68580b9b2a5566fe9871e0506752286b75d3707c
Mohamed Amine <delhemamine@gmail.com>
2026-04-27 09:58:05 +0200

    Ajout des config (db, proxmox, redis, stripe)

---
+++ backend/src/config/proxmox.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  proxmox_url: process.env.PROXMOX_URL,
+  default_node: process.env.PROXMOX_DEFAULT_NODE,
+  token_id: process.env.PROXMOX_TOKEN_ID,
+  token_secret: process.env.PROXMOX_TOKEN_SECRET
+};
---
+++ backend/src/config/stripe.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stripe_secret : process.env.STRIPE_SECRET_KEY,
+  stripe_webhook_secret : process.env.STRIPE_WEBHOOK_SECRET
+};
