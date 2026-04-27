c042fd56c05c557a656eada41142260ee6f5cd42
Mohamed Amine DELHEM <152877088+DELHEMMOHAMED@users.noreply.github.com>
2026-04-27 09:25:20 +0200

    Delete backend directory

--- backend/routes/availability.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const availCtrl = require('../controllers/availability');
-
-router.get('/memory', availCtrl.getMemory);
-router.get('/cpu', availCtrl.getCPU);
-router.get('/storage', availCtrl.getStorage);
-
-module.exports = router;
